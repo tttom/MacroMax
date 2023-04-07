ce3af23b6165ba3e2f339672c0b8b2f52509d65d
Tom Vettenburg <tom.vettenburg@gmail.com>
2023-04-07 22:48:52 +0100

    Cleaning up redundant code in documentation generation.

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -19,6 +19,7 @@ code_path = Path(__file__).parent.parent.absolute()
 code_path_on_rtd = Path.cwd().absolute() / 'python'
 sys.path.insert(0, str(code_path))
 sys.path.insert(0, str(code_path_on_rtd))
+sys.path.insert(0, str(Path.cwd().absolute()))
 
 
 # -- Project information -----------------------------------------------------
